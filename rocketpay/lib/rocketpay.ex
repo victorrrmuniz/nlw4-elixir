5dd4109cf3e5bc2f60e36011af4b7c58fbfbc195
Víctor Muniz dos Santos <victor.santos@ituran.com.br>
2021-02-24 09:21:19 -0300

    Dia 2 - criaçao da migration e endpoint users

--- rocketpay/lib/rocketpay.ex
+++ rocketpay/lib/rocketpay.ex
@@ -1,9 +1,4 @@
 defmodule Rocketpay do
-  @moduledoc """
-  Rocketpay keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  alias Rocketpay.Users.Create, as: UseCreate
+  defdelegate create_user(params), to: UseCreate, as: :call
 end
---
+++ rocketpay/lib/rocketpay/users/create.ex
@@ -0,0 +1,11 @@
+
+defmodule Rocketpay.Users.Create do
+  alias Rocketpay.{Repo, User}
+
+  def call(params) do
+    params
+    |> User.changeset()
+    |> Repo.insert()
+  end
+
+end
